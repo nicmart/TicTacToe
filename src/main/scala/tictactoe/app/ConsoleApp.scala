2e33ad94f7900a0d5496e97639c62cf20b68148d
Nicolò Martini <nicolo@martini.io>
2019-06-02 15:16:44 +0100

    Implement BeautifulBoardStringView

--- src/main/scala/tictactoe/app/ConsoleApp.scala
+++ src/main/scala/tictactoe/app/ConsoleApp.scala
@@ -1,21 +1,21 @@
 package tictactoe.app
 
 import scalaz.zio.App
+import tictactoe.beautifulstringview.BeautifulBoardStringView
 import tictactoe.consolegameevents._
 import tictactoe.consolemovessource.ConsoleMovesSource
 import tictactoe.domain.game.model.{Board, StandardGame}
 import tictactoe.domain.runner.GameRunner
 import tictactoe.rudegamestrings.RudeConsoleGameStrings
-import tictactoe.simplestringview.SimpleBoardStringView
 import tictactoe.stringpresenter.BoardStringPresenter
 
 object ConsoleApp extends App {
   val runner: GameRunner =
     GameRunner(
-      StandardGame.newGame(Board.Size(3)),
+      StandardGame.newGame(Board.Size(6)),
       new ConsoleGameEvents(
         new BoardStringPresenter(_.fold("🖕", "🧠")),
-        new SimpleBoardStringView,
+        new BeautifulBoardStringView,
         RudeConsoleGameStrings
       ),
       new ConsoleMovesSource,
---
+++ src/test/scala/tictactoe/consolegameevents/BeatifulBoardStringViewTest.scala
@@ -0,0 +1,27 @@
+package tictactoe.consolegameevents
+
+import tictactoe.beautifulstringview.BeautifulBoardStringView
+import tictactoe.domain.CommonTest
+import tictactoe.stringpresenter.BoardStringViewModel
+
+class BeatifulBoardStringViewTest extends CommonTest {
+  "A BeautifulBoardView" - {
+    "should render an empty board" in {
+      val boardViewModel = BoardStringViewModel.empty(3, "?")
+      val renderedBoard = view.render(boardViewModel)
+      val expectedRenderedBoard =
+        """>┌───┬───┬───┐
+           >│ ? │ ? │ ? │
+           >├───┼───┼───┤
+           >│ ? │ ? │ ? │
+           >├───┼───┼───┤
+           >│ ? │ ? │ ? │
+           >└───┴───┴───┘"""
+          .stripMargin('>')
+
+      renderedBoard shouldBe expectedRenderedBoard
+    }
+  }
+
+  lazy val view = new BeautifulBoardStringView
+}
