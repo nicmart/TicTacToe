fbc0479dad7465ad0e225404f3c4378ba9751c76
Scala steward <nicolo@martini.io>
2019-06-27 14:06:25 +0100

    Update cats-core to 2.0.0-M4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,6 +4,6 @@ object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
   lazy val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.14.0"
   lazy val zio = "org.scalaz" %% "scalaz-zio" % "1.0-RC5"
-  lazy val cats = "org.typelevel" %% "cats-core" % "2.0.0-M1"
+  lazy val cats = "org.typelevel" %% "cats-core" % "2.0.0-M4"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "1.3.1"
 }
