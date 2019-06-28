a49856c23061fa8e3d215c41fb46351c23e45471
Scala steward <contenthub@springernature.com>
2019-06-28 10:36:54 +0000

    Update scalatest to 3.0.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.8"
   lazy val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.14.0"
   lazy val zio = "org.scalaz" %% "scalaz-zio" % "1.0-RC5"
   lazy val cats = "org.typelevel" %% "cats-core" % "2.0.0-M1"
